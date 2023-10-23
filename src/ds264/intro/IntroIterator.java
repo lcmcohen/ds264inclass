5c4960cd42692e2080a9feb5bf8601153ef3b196
lcmcohen <michael.cohen68@touro.edu>
2023-10-23 13:42:10 -0400

    Update IntoIterator to demo use of iter.remove(). Add example of an Iterable implementation.

--- src/ds264/intro/IntroIterator.java
+++ src/ds264/intro/IntroIterator.java
@@ -3,6 +3,7 @@ package ds264.intro;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class IntroIterator {
 
@@ -64,8 +65,9 @@ public class IntroIterator {
             String student = (String) sIter.next();
             System.out.println("RE-ITER - Da next one is: " + student);
             /*** remove CURRENT one - based on  VALUE retrieved */
-             if (student.equals("Buddy Rich"))         // also demo prob with 0, Gene
-                studentNames.remove(student);  /*prob*/   // also demo prob with 0, Gene
+             if (student.equals("Gene Krupa"))         // also demo prob with 0, Gene
+                // BAD studentNames.remove(0);  /*prob*/   // also demo prob with 0, Gene
+                 sIter.remove();
              /**/
         }
 
@@ -73,6 +75,50 @@ public class IntroIterator {
 
 
 
+        Lander data = new Lander("abcdefg");
+
+         for (Object s: data) {
+             System.out.println("Lander iterator = next is: " + s);
+
+         }
+
+
     }
 
+    private static class Lander implements Iterable {
+        String internalData;
+        int cursor = 0;
+        public Lander(String str) {
+            this.internalData = str;
+        }
+
+
+        @Override
+        public Iterator iterator() {
+            return new Iterator() {
+                @Override
+                public boolean hasNext() {
+                    return cursor < internalData.length();
+                }
+
+                @Override
+                public Object next() {
+                    System.out.println("iterator " + this.getClass() + " was called when cursor = " + cursor);
+                    return internalData.toCharArray()[cursor++];
+                }
+
+                @Override
+                public void remove() {
+                    Iterator.super.remove();
+                }
+
+                @Override
+                public void forEachRemaining(Consumer action) {
+                    Iterator.super.forEachRemaining(action);
+                }
+            };
+
+
+        }
+    }
 }
